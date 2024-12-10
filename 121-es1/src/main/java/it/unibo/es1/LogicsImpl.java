cecc355dd0f957bdfebbb07d639cd2d016c59536
Giulia Fares - giulia.fares@studio.unibo.it <giulia.fares@studio.unibo.it>
2024-12-10 17:00:42 +0100

    Finished exercise 121

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,55 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class LogicsImpl implements Logics {
 
-	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+	private List<Integer> values;
+	
+	public LogicsImpl(final int size) {
+		this.values = new ArrayList<>(Collections.nCopies(size, 0));
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.values.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return this.values;
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return values.stream()
+			.map(d -> d != values.size())
+			.toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		if (this.enablings().get(elem)) {
+			int value = values.get(elem);
+			values.set(elem, ++value);
+		}
+		return this.values.get(elem);
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return this.values.stream()
+			.map(d -> d.toString())
+			.collect(Collectors.joining("|", "<<", ">>"));
 	}
-
+	
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return values.stream()
+			.allMatch(i -> i == this.values.get(0));
 	}
 }
